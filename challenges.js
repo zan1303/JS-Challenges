be30c056e6891dba1b2ae981183110cf03d999ec
Žan <zan1303@hotmail.com>
2025-01-13 23:28:40 +0100

    adding challenge number 9.

---
+++ challenges.js
@@ -0,0 +1,18 @@
+
+
+//1.
+
+
+//9. Return a Boolean if a number is divisible by 10
+
+//Change the "number" variable to get the TRUE/FALSE statement
+const isDivisibleByTen = () => {
+    const number = 500;
+    if(number % 10 === 0) {
+        console.log("True");
+    }
+    else {
+        console.log("False");
+    }
+}
+isDivisibleByTen();
