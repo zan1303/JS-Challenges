75af7331e78237b0a043d9adb1e37d456bd9185a
Žan <zan1303@hotmail.com>
2025-01-15 23:23:47 +0100

    adding challenge 10.

--- challenges.js
+++ challenges.js
@@ -16,3 +16,22 @@ const isDivisibleByTen = () => {
     }
 }
 isDivisibleByTen();
+
+//10. Return the number of vowels in a string
+
+let string = "I'm a developer";
+
+const vowelCounter = () => {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+
+    for (let char of string.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count += 1; 
+        }
+    }
+
+    return count > 0 ? `Number of vowels: ${count}` : "No vowels found";
+};
+
+console.log(vowelCounter());
